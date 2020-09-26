111991a6ee5837b7bb096be8c65d749437724e68
Teknical Mage <arcazjordan@gmail.com>
2020-09-25 21:23:07 -0400

    mbnb
    b nm b

--- bot.py
+++ bot.py
@@ -1,66 +1,112 @@
-#Made by Teknical Mage 10/23/18
 import discord
 
 from discord.ext import commands
-from discord.ext.commands import Bot
 import asyncio
 
 #Your Discord Bots Token goes here. You can individually put it in every time you need it, but this alos works
 TOKEN = ''
 
-players = {}
    
 #Your going to start all of your commands with this
-bot = commands.Bot(command_prefix="#")
+bot = commands.Bot(command_prefix="`")
 
 #First thing that will run upon starting the script
-@bot.event 
-async def on_ready():
-    print ("Good to Go")
-    print ("I am running on " + bot.user.name)
-    print ("with the ID" + bot.user.id)
-    
-#Example of the bot responding to a command    
-@bot.command(pass_context=True)
-async def ping(ctx):
-    await bot.say("ping")
-    print ("user has pinged")
 
-#Joins Voice Channel    
-@bot.command(pass_context=True)
-async def join(ctx):
-    channel = ctx.message.author.voice.voice_channel
-    await bot.join_voice_channel(channel)
 
-#Leave Voice Channel        
-@bot.command(pass_context=True)
-async def leave(ctx):
-    server = ctx.message.server
-    voice_client = bot.voice_client_in(server)
-    await voice_client.disconnect()
-    
-#Playing a youtube link
-@bot.command(pass_context=True)
-async def play(ctx, url): 
-    server = ctx.message.server
-    voice_client = bot.voice_client_in(server)
-    player = await voice_client.create_ytdl_player(url)
-    players[server.id] = player
-    player.start()
 
-#Basically #Help, but not dumb        
-@bot.command(pass_context=True)
-async def commands(ctx):
-    await bot.say("#help - get a list of commands \n#ping - ping the bot \n#echo - repeat a line you want \n#join - join the server \n#leave - leave the server")   
+@bot.event
+async def on_voice_state_update(member, before, after):
+    x = before
+    y = after
+    print(x)
+    print('_______________________---')
+    print(y)
+    print("Ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
 
-#Another text test    
+#@bot.event
+#async def on_voice_state_update(member, before, after):
+    #if before.channel is None and after.channel is not None:
+        #if after.channel.id == [481263163753496590]:
+            #await member.guild.system_channel.send("Alarm!")
+
 @bot.command()
-async def echo(*args):
-    output = ''
-    for word in args:
-        output += word
-        output += ' '
-    await bot.say(output)
+async def ping(ctx):
+    await ctx.send('pong')
+    
+###########################################
+@bot.command()
+async def showe(ctx):
+    member_list = ' '
+    for member in ctx.message.guild.members:
+        member_list += member.name + ' '
+        
+    print(member_list)
+###########################################
+
+@bot.command(pass_context=True)
+async def test(ctx, arg):
+    await ctx.send(arg)
+   
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+
+###########################################
+@bot.command()
+async def grab(ctx):
+    #member = ctx.message.guild.get_member(362748893022126091)
+    #channel = ctx.member.voice.channel
+    print(ctx.guild.voice_channels)
+
+    #print(ctx.guild.voice_channels)
+###########################################
+
+    
+@bot.command()
+async def general(ctx, member: discord.Member):
+    #print(ctx.author.voice.channel.id)
+    x = bot.get_channel(481263163753496590)
+    
+    await member.move_to(x)
+@bot.command()
+async def Gen1(ctx, member: discord.Member):
+    #print(ctx.author.voice.channel.id)
+    x = bot.get_channel(758379379365118054)
+    
+    await member.move_to(x)
+
+
+@bot.command()
+async def team(ctx):
+    vc = ctx.guild.get_channel(481263163753496590)
+    yuh = []
+
+    for (i) in vc.members:
+        yuh.append(i)
+    
+    print(yuh)
+    Current = (len(yuh))
+
+    ChannelMax=2
+
+    if Current > ChannelMax:    
+        await ctx.send("A SACRIFICE MUST BE MADE")
+    else:
+        await ctx.send("Actually go fuck yourself")
+        
+
+
+
+    #while x > 50000:
+        #print('false')
+        
+    #print(vc.members)
+ 
 
 #Runs the Bot       
 bot.run(TOKEN)
